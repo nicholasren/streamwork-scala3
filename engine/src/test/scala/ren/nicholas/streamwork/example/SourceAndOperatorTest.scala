228ddb3d34728ccf5e400a8663fe308b19606a2f
xiaojun.ren <xiaojun.z.ren@tmr.qld.gov.au>
2022-06-30 19:39:16 +1000

    Additional operator

--- engine/src/test/scala/ren/nicholas/streamwork/example/SourceAndOperatorTest.scala
+++ engine/src/test/scala/ren/nicholas/streamwork/example/SourceAndOperatorTest.scala
@@ -1,27 +1,30 @@
 package ren.nicholas.streamwork.example
 
+import org.scalatest.BeforeAndAfter
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should
 import ren.nicholas.streamwork.core.stream.{Sink, Source, StreamBuilder}
 import ren.nicholas.streamwork.core.topology.Topology
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import scala.concurrent.duration.SECONDS
+import scala.concurrent.duration.{Duration, SECONDS}
 
-class SourceAndOperatorTest extends AnyFunSpec with should.Matchers {
+class SourceAndOperatorTest extends AnyFunSpec with should.Matchers with BeforeAndAfter {
+  var source: Source[Int] = _
+  var streamBuilder: StreamBuilder = _
+
+  before {
+    source = Source.of(1, 2, 3, 4)
+    streamBuilder = StreamBuilder()
+  }
 
   describe("simple stream run") {
     it("should read from source and send to sink") {
-      val source: Source[Int] = Source.of(1, 2, 3, 4)
       val sink: Sink[Int] = Sink.empty()
-
-      val streamBuilder: StreamBuilder = StreamBuilder()
 
       streamBuilder
         .source("numbers", source)
         .to("result", sink)
-
       val topology = streamBuilder.build()
       topology.run()
       Thread.sleep(1000)
@@ -30,10 +33,7 @@ class SourceAndOperatorTest extends AnyFunSpec with should.Matchers {
     }
 
     it("should read from source and apply operator and send to sink") {
-      val source: Source[Int] = Source.of(1, 2, 3, 4)
       val sink: Sink[Int] = Sink.empty()
-
-      val streamBuilder: StreamBuilder = StreamBuilder()
 
       streamBuilder
         .source("numbers", source)
@@ -45,6 +45,21 @@ class SourceAndOperatorTest extends AnyFunSpec with should.Matchers {
       Thread.sleep(1000)
 
       sink.all should contain allOf(2, 4, 6, 8)
+    }
+
+    it("should read from source and apply operators and send to sink") {
+      val sink: Sink[String] = Sink.empty()
+
+      streamBuilder
+        .source("numbers", source)
+        .map("double", _ * 2)
+        .map("to_string", _.toString)
+        .to("result", sink)
+      val topology = streamBuilder.build()
+      topology.run()
+      Thread.sleep(1000)
+
+      sink.all should contain allOf("2", "4", "6", "8")
     }
   }
 }
