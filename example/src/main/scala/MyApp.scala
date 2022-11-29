e16a441891cdcff1d15b7d26d269072b34adadbb
REN Xiaojun <nicholas.x.ren@gmail.com>
2022-11-29 22:09:58 +1100

    Changed stream topology

--- example/src/main/scala/MyApp.scala
+++ example/src/main/scala/MyApp.scala
@@ -1,35 +1,21 @@
 import ren.nicholas.streamwork.core.stream.*
 import ren.nicholas.streamwork.core.topology.Topology
 import com.github.javafaker.Faker
-import scala.util.Random.{nextInt}
-
-case class Person(name: String, age: Int, isHost: Boolean)
 
 object MyApp extends App {
   private val streamBuilder = StreamBuilder()
 
   private val faker = Faker()
 
-  val people = List(
-    Person("Bernard", 55, true),
-    Person("Dolores", 35, true),
-    Person("Maeve", 5, true),
-    Person("Liam", 55, false),
-    Person("Teddy", 35, true)
-  )
-
-
   streamBuilder
-    .source("source", Source.continually {
-      Person("1", nextInt, true)
-    }, 2)
-    .map("change age", p => Person(p.name, nextInt(100), nextInt(100) % 3 == 0))
-    .filter("is host", _.isHost)
+    .source("source", Source.continually(faker.name().name()))
+    .map("to person", name => Person(name, faker.number().numberBetween(5, 100)))
     .to("print", Sink.console())
 
   val topology: Topology = streamBuilder.build()
+
   topology.run()
 
-  Thread.sleep(30000)
+  Thread.sleep(3000)
   topology.stop()
 }
